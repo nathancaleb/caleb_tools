395c47235533eaa0eff4f9b6d1acb96ef608e3a2
Nathan Caleb <nathancalebss@gmail.com>
2023-09-01 15:30:15 -0300

    fix css and script path

--- script/index_script.js
+++ script/index_script.js
@@ -3,6 +3,6 @@ const mainScreen = document.querySelector(".main_container")
 const calculatorAppBtn = document.querySelector("#calculator_app_button")
 
 calculatorAppBtn.addEventListener("click", function(){
-    $(mainScreen).load('./apps/calculator.html');
+    $(mainScreen).load('apps/calculator.html');
     document.title = document.title + " - Calculator"
 })
