9545137e28d72d08ad9df0e2683342dbb2589271
Nathan Caleb <nathancalebss@gmail.com>
2023-09-12 01:02:57 -0300

    test with scripts path

--- script/index_script.js
+++ script/index_script.js
@@ -1,3 +1,4 @@
+const mainScript = document.querySelector("#main_script")
 const mainScreen = document.querySelector(".main_container")
 
 const calculatorAppBtn = document.querySelector("#calculator_app_button")
@@ -5,12 +6,20 @@ const calendarAppBtn = document.querySelector("#calendar_app_button")
 
 calculatorAppBtn.addEventListener("click", function(){
     $(mainScreen).load('apps/calculator.html');
-    document.title = document.title + " - Calculator"
+    document.title = document.title + " - Calculator";
+    // const otheScript = document.createElement("script");
+    // mainScript.insertBefore(otheScript);
+    // otheScript.setAttribute("src", "script/calculator_script.js") 
 })
 
 calendarAppBtn.addEventListener("click", function(){
     $(mainScreen).load('apps/calendar.html');
     document.title = document.title + " - Calendar"
+    // const otheScript = document.createElement("script");
+    // mainScript.insertBefore(otheScript);
+    // otheScript.setAttribute("src", "script/calendar_script.js") 
 })
 
 
+{/* <script src="script/calculator_script.js"></script>
+<script src="script/calendar_script.js"></script> */}
