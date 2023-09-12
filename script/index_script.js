0d694ccf57c0a8c1410bf2d295fc524daa1da5f3
Nathan Caleb <nathancalebss@gmail.com>
2023-09-12 00:14:06 -0300

    finish Calendar, add index menu, calc color change

--- script/index_script.js
+++ script/index_script.js
@@ -1,8 +1,16 @@
 const mainScreen = document.querySelector(".main_container")
 
 const calculatorAppBtn = document.querySelector("#calculator_app_button")
+const calendarAppBtn = document.querySelector("#calendar_app_button")
 
 calculatorAppBtn.addEventListener("click", function(){
     $(mainScreen).load('apps/calculator.html');
     document.title = document.title + " - Calculator"
 })
+
+calendarAppBtn.addEventListener("click", function(){
+    $(mainScreen).load('apps/calendar.html');
+    document.title = document.title + " - Calendar"
+})
+
+
